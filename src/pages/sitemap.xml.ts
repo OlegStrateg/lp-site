e67ae83909c8688123fa511491d46a8b0c6c6e94
OlegStrateg <ceo@pumbie.ru>
2026-08-31 15:22:58 +0300

    LP-022: добавить 52 Pinterest локали в sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -7,6 +7,61 @@ import { getCollection } from 'astro:content';
 export const prerender = true;
 
 
+const PINTEREST_LOCALE_PATHS = [
+  "/pinterest-downloader/",
+  "/am/pinterest-downloader/",
+  "/ar/pinterest-downloader/",
+  "/bg/pinterest-downloader/",
+  "/bn/pinterest-downloader/",
+  "/ca/pinterest-downloader/",
+  "/cs/pinterest-downloader/",
+  "/da/pinterest-downloader/",
+  "/de/pinterest-downloader/",
+  "/el/pinterest-downloader/",
+  "/es/pinterest-downloader/",
+  "/es-419/pinterest-downloader/",
+  "/et/pinterest-downloader/",
+  "/fa/pinterest-downloader/",
+  "/fi/pinterest-downloader/",
+  "/fil/pinterest-downloader/",
+  "/fr/pinterest-downloader/",
+  "/gu/pinterest-downloader/",
+  "/he/pinterest-downloader/",
+  "/hi/pinterest-downloader/",
+  "/hr/pinterest-downloader/",
+  "/hu/pinterest-downloader/",
+  "/id/pinterest-downloader/",
+  "/it/pinterest-downloader/",
+  "/ja/pinterest-downloader/",
+  "/kn/pinterest-downloader/",
+  "/ko/pinterest-downloader/",
+  "/lt/pinterest-downloader/",
+  "/lv/pinterest-downloader/",
+  "/ml/pinterest-downloader/",
+  "/mr/pinterest-downloader/",
+  "/ms/pinterest-downloader/",
+  "/nl/pinterest-downloader/",
+  "/no/pinterest-downloader/",
+  "/pl/pinterest-downloader/",
+  "/pt-br/pinterest-downloader/",
+  "/pt-pt/pinterest-downloader/",
+  "/ro/pinterest-downloader/",
+  "/ru/pinterest-downloader/",
+  "/sk/pinterest-downloader/",
+  "/sl/pinterest-downloader/",
+  "/sr/pinterest-downloader/",
+  "/sv/pinterest-downloader/",
+  "/sw/pinterest-downloader/",
+  "/ta/pinterest-downloader/",
+  "/te/pinterest-downloader/",
+  "/th/pinterest-downloader/",
+  "/tr/pinterest-downloader/",
+  "/uk/pinterest-downloader/",
+  "/vi/pinterest-downloader/",
+  "/zh-cn/pinterest-downloader/",
+  "/zh-tw/pinterest-downloader/"
+];
+
 const STATIC_PATHS = ['/', '/convert/', '/about/', '/extensions/', '/extensions/picture-converter/', '/privacy/', '/terms/', '/formats/', '/guides/'];
 
 export const GET: APIRoute = async () => {
@@ -14,6 +69,7 @@ export const GET: APIRoute = async () => {
   const articles = await getCollection('articles');
   const paths = [
     ...STATIC_PATHS,
+    ...PINTEREST_LOCALE_PATHS,
     ...converters.map((c) => `/convert/${c.data.key}/`).sort(),
     ...articles.map((a) => `/${a.data.section}/${a.data.slug}/`).sort(),
   ];
