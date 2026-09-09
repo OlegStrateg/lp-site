c1808ed39110c930d06e08dec4f5477d2edf0037
OlegStrateg <ceo@pumbie.ru>
2026-09-09 21:08:04 +0300

    LP-082: add PT-BR PSD to PNG to sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -72,6 +72,7 @@ const LOCALIZED_EXACT_CONVERTER_PATHS = [
   '/pt-br/convert/png-to-psd/',
   '/pt-br/convert/jpg-to-psd/',
   '/pt-br/convert/psd-to-jpg/',
+  '/pt-br/convert/psd-to-png/',
 ];
 
 const STATIC_PATHS = ['/', '/ru/', '/de/', '/es/', '/fr/', '/pt-br/', '/ja/', '/zh-cn/', '/about/', '/extensions/', '/ru/extensions/', '/de/extensions/', '/es/extensions/', '/fr/extensions/', '/pt-br/extensions/', '/ja/extensions/', '/zh-cn/extensions/', '/privacy/', '/terms/', '/formats/', '/guides/'];
